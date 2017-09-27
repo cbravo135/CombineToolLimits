0dca53e511c86a6db219f34b11fa885465bb8a27
cbravo135 <c.bravo@cern.ch>
2017-09-27 15:04:05 +0200

    Update to new formats

--- GetAsymptoticLimits.sh
+++ GetAsymptoticLimits.sh
@@ -1,8 +1,8 @@
 #bin/bash
 #declare -a multi=("2" "3" "4" "5" "6" "7" "8")
-declare -a multi=("5" "6" "7" "8" "9" "10")
+declare -a multi=("3" "4" "5" "6" "7" "8" "9" "10" "11")
 #declare -a stmin=("4500")
-START=2700
+START=3000
 END=8000
 STEP=100
 for j in "${multi[@]}" 
@@ -17,7 +17,7 @@ touch OutputLogFiles/asympLimit_mul${j}.log
 	   elif [ $i -lt "4500" ]; then 
 		combine -M Asymptotic DataCards/CardFiles_Mul${j}/realistic-counting-experiment_ST${i}_BHMI.txt --setPhysicsModelParameters r=0.1 --setPhysicsModelParameterRanges r=0,100 |grep 'r <' |cut -d "<" -f 2| echo $i `tr '\n' ' '` >> OutputLogFiles/asympLimit_mul${j}.log 
 	   else 
-	        combine -M Asymptotic DataCards/CardFiles_Mul${j}/realistic-counting-experiment_ST${i}_BHMI.txt --setPhysicsModelParameters r=0.1 --setPhysicsModelParameterRanges r=0,5 | grep 'r <' |cut -d "<" -f 2| echo $i `tr '\n' ' '` >> OutputLogFiles/asympLimit_mul${j}.log
+	        combine -M Asymptotic DataCards/CardFiles_Mul${j}/realistic-counting-experiment_ST${i}_BHMI.txt --setPhysicsModelParameters r=0.1 --setPhysicsModelParameterRanges r=0,5 | grep 'r <' |cut -d "<" -f 2| echo $i `tr '\n' ' '` >> OutputLogFiles/asympLimit_mul${j}.log 
 	   fi
         rm higgsCombineTest.Asymptotic*root
 	done
